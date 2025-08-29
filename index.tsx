605df0248e2dfe12f16628cecc284e520409c81a
cobrynet <info.cobrynet@gmail.com>
2025-08-30 01:00:17 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "../tailwind.css";
-import { PcEsportazione } from "./screens/PcEsportazione/PcEsportazione";
 
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <PcEsportazione />
-  </StrictMode>,
-);
+function Test() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold text-indigo-600">Hello from Vite ✅</h1>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("app") as HTMLElement).render(<Test />);
