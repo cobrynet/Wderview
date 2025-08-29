3dc13dd7e3541a49f2c82c4fe40aacf1fd2ccf55
cobrynet <info.cobrynet@gmail.com>
2025-08-30 01:18:25 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,8 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import "../tailwind.css";
 
-function Test() {
-  return (
-    <div className="min-h-screen grid place-items-center">
-      <h1 className="text-4xl font-bold text-indigo-600">Hello from Vite ✅</h1>
-    </div>
-  );
-}
-
-createRoot(document.getElementById("app") as HTMLElement).render(<Test />);
+createRoot(document.getElementById("app") as HTMLElement).render(
+  <div className="min-h-screen grid place-items-center">
+    <h1 className="text-4xl font-bold text-indigo-600">Hello from Vite ✅</h1>
+  </div>
+);
